762a2d368ae2475f9b3ca4f069ca20b144411214
DaGo85 <davidgoe85@gmail.com>
2022-10-27 15:24:00 +0200

    added first chart version

--- src/app/components/chart/chart.component.ts
+++ src/app/components/chart/chart.component.ts
@@ -13,178 +13,129 @@ interface Stock {
 })
 export class ChartComponent implements OnInit {
   public title = 'Line Chart';
+
   dates: any[] = [
-    new Date('2010-01-01'),
-    new Date('2010-01-02'),
-    new Date('2010-01-03'),
-    new Date('2010-01-04'),
-    new Date('2010-01-05'),
-    new Date('2010-01-06'),
-    new Date('2010-01-07'),
-    new Date('2010-01-08'),
-    new Date('2010-01-09'),
-    new Date('2010-01-10'),
+    1666872879, 1666872890, 1666872891, 1666872892, 1666872893, 1666872894,
+    1666872895, 1666872896, 1666872897,
   ];
 
-  public data = [
-    {
-      value: 20,
-      date: '2020-05-12T12:19:00+00:00',
-    },
-    {
-      value: 50,
-      date: '2020-05-14T12:19:00+00:00',
-    },
-    {
-      value: 30,
-      date: '2020-05-16T12:19:00+00:00',
-    },
-    {
-      value: 80,
-      date: '2020-05-18T12:19:00+00:00',
-    },
-    {
-      value: 55,
-      date: '2020-05-20T12:19:00+00:00',
-    },
-    {
-      value: 60,
-      date: '2020-05-22T12:19:00+00:00',
-    },
-    {
-      value: 45,
-      date: '2020-05-24T12:19:00+00:00',
-    },
-    {
-      value: 30,
-      date: '2020-05-26T12:19:00+00:00',
-    },
-    {
-      value: 40,
-      date: '2020-05-28T12:19:00+00:00',
-    },
-    {
-      value: 70,
-      date: '2020-05-30T12:19:00+00:00',
-    },
-    {
-      value: 63,
-      date: '2020-06-01T12:19:00+00:00',
-    },
-    {
-      value: 40,
-      date: '2020-06-03T12:19:00+00:00',
-    },
-    {
-      value: 50,
-      date: '2020-06-05T12:19:00+00:00',
-    },
-    {
-      value: 75,
-      date: '2020-06-07T12:19:00+00:00',
-    },
-    {
-      value: 20,
-      date: '2020-06-09T12:19:00+00:00',
-    },
-    {
-      value: 50,
-      date: '2020-06-11T12:19:00+00:00',
-    },
-    {
-      value: 80,
-      date: '2020-06-13T12:19:00+00:00',
-    },
-    {
-      value: 75,
-      date: '2020-06-15T12:19:00+00:00',
-    },
-    {
-      value: 82,
-      date: '2020-06-17T12:19:00+00:00',
-    },
-    {
-      value: 55,
-      date: '2020-06-19T12:19:00+00:00',
-    },
-    {
-      value: 35,
-      date: '2020-06-21T12:19:00+00:00',
-    },
-    {
-      value: 34,
-      date: '2020-06-23T12:19:00+00:00',
-    },
-    {
-      value: 45,
-      date: '2020-06-25T12:19:00+00:00',
-    },
-    {
-      value: 58,
-      date: '2020-06-27T12:19:00+00:00',
-    },
-    {
-      value: 34,
-      date: '2020-06-29T12:19:00+00:00',
-    },
-    {
-      value: 60,
-      date: '2020-07-01T12:19:00+00:00',
-    },
-    {
-      value: 75,
-      date: '2020-07-03T12:19:00+00:00',
-    },
-    {
-      value: 80,
-      date: '2020-07-05T12:19:00+00:00',
-    },
-    {
-      value: 29,
-      date: '2020-07-07T12:19:00+00:00',
-    },
-    {
-      value: 40,
-      date: '2020-07-09T12:19:00+00:00',
-    },
-    {
-      value: 54,
-      date: '2020-07-11T12:19:00+00:00',
-    },
-    {
-      value: 67,
-      date: '2020-07-13T12:19:00+00:00',
-    },
-    {
-      value: 90,
-      date: '2020-07-15T12:19:00+00:00',
-    },
-    {
-      value: 84,
-      date: '2020-07-17T12:19:00+00:00',
-    },
-    {
-      value: 43,
-      date: '2020-07-19T12:19:00+00:00',
-    },
-  ];
+  temps: any[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-  temp: any[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  public fullData: { date: number; value: number }[] = [];
 
   margin = { top: 40, right: 48, bottom: 40, left: 40 };
   width: number = 600;
   height: number = 620 - this.margin.top - this.margin.bottom;
-  viewBoxWidth: number = this.width + this.margin.left + this.margin.right;
-  viewBoxHeight: number = this.height + this.margin.top + this.margin.bottom;
+  viewBoxWidth = this.width + this.margin.left + this.margin.right;
+  viewBoxHeight = this.height + this.margin.top + this.margin.bottom;
+
+  draw() {
+    console.log(this.fullData);
+    let svg = d3
+      .select('#my_dataviz')
+      .attr('viewBox', [0, 0, this.viewBoxWidth, this.viewBoxHeight])
+      .append('g')
+      .attr('transform', `translate(${this.margin.left}, ${this.margin.top})`);
+
+    // x axis
+    // @ts-ignore
+    let x = d3.scaleTime().domain(d3.extent(this.dates)).range([0, this.width]);
+
+    const xaxis = d3.axisBottom(x).tickFormat(d3.format('0'));
+    //.scale(x);
+
+    svg
+      .append('g')
+      .attr('transform', 'translate(0,' + this.height + ')')
+      .call(xaxis);
+
+    //y axis
+
+    let y = d3
+      .scaleLinear()
+      // @ts-ignore
+      .domain([d3.min(this.temps) - 2, d3.max(this.temps) + 2])
+      .range([this.height, 0]);
+
+    svg.append('g').call(d3.axisLeft(y));
+
+    // Graph Label
+    svg
+      .append('text')
+      .style('font', '30px times')
+      .attr('class', 'label-title')
+      .attr('x', this.width / 2)
+      .attr('y', -this.margin.top / 8)
+      .attr('text-anchor', 'middle')
+      .text('Temp. for the next 48 hours');
+
+    //  X axis label
+    svg
+      .append('text')
+      .style('font', '22px times')
+      .attr('class', 'label')
+      .attr('x', this.width)
+      .attr('y', this.height + this.margin.bottom)
+      .attr('text-anchor', 'middle')
+      .text('Time');
+
+    //  Y axis label
+    svg
+      .append('text')
+      .style('font', '22px times')
+      .attr('class', 'label')
+      .attr('x', 0 - this.margin.left)
+      .attr('y', 0 - this.margin.top / 2)
+      .text('Temp. in °C');
+
+    let line = svg
+      .append('path')
+      .datum(this.temps)
+      .attr('class', 'line')
+      .attr(
+        'd',
+        // @ts-ignore
+        d3
+          .line() // @ts-ignore
+          .x((d) => x(d.date)) // @ts-ignore
+          .y((d) => y(d.value))
+      );
+
+    // @ts-ignore
+    svg
+      .append('path')
+      .datum(this.fullData)
+      .attr('fill', 'none')
+      .attr('stroke', 'steelblue')
+      .attr('stroke-width', 1.5)
+      .attr(
+        'd',
+        // @ts-ignore
+        d3
+          .line() // @ts-ignore
+          .x((d) => x(d.date)) // @ts-ignore
+          .y((d) => y(d.value))
+      );
+
+    // Create Dots
+    const circles = svg
+      .append('g')
+      .selectAll('circle')
+      .data(this.fullData)
+      .enter()
+      .append('circle')
+      .attr('cx', (d) => x(d.date))
+      .attr('cy', (d) => y(d.value))
+      .attr('r', 5)
+      .attr('class', 'dot');
+  }
 
   constructor() {}
-  public ngOnInit(): void {}
 
-  svg = d3
-    .select('svg')
-    .attr('viewBox', [0, 0, this.viewBoxWidth, this.viewBoxHeight])
-    .append('g')
-    .attr('transform', `translate(${this.margin.left}, ${this.margin.top})`);
-
-  // Create Axis
+  public ngOnInit(): void {
+    this.dates.map((date, i) => {
+      this.fullData.push({ date: date, value: this.temps[i] });
+    });
+    this.draw();
+  }
 }
